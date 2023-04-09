9022a106c0cc77c8fe4a3043747af01a3aeff9c4
philpen <159920091+philpen@users.noreply.github.com>
2023-04-09 12:07:44 +0000

    Lines authored by dnyy

---
+++ prepro_tinyshakespeare.py
@@ -0,0 +1,8 @@
+
+"""
+Downloads and tokenizes the TinyShakespeare dataset.
+- The download is from Github.
+- The tokenization is GPT-2 tokenizer with tiktoken
+
+The output is written to a newly created data/ folder.
+The script prints:
