42f16765e1251d58ad64c8493313c0a00890a792
philpen <159920091+philpen@users.noreply.github.com>
2023-04-16 10:47:15 +0000

    Update image.php

--- prepro_tinyshakespeare.py
+++ prepro_tinyshakespeare.py
@@ -35,3 +35,41 @@ def download_file(url: str, fname: str, chunk_size=1024):
         total=total,
         unit="iB",
         unit_scale=True,
+        unit_divisor=1024,
+    ) as bar:
+        for data in resp.iter_content(chunk_size=chunk_size):
+            size = file.write(data)
+            bar.update(size)
+
+def download():
+    """Downloads the TinyShakespeare dataset to DATA_CACHE_DIR"""
+    os.makedirs(DATA_CACHE_DIR, exist_ok=True)
+
+    # download the TinyStories dataset, unless it's already downloaded
+    data_url = "https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt"
+    data_filename = os.path.join(DATA_CACHE_DIR, "tiny_shakespeare.txt")
+    if not os.path.exists(data_filename):
+        print(f"Downloading {data_url} to {data_filename}...")
+        download_file(data_url, data_filename)
+    else:
+        print(f"{data_filename} already exists, skipping download...")
+
+def tokenize():
+    eot = enc._special_tokens['<|endoftext|>'] # end of text token
+    data_filename = os.path.join(DATA_CACHE_DIR, "tiny_shakespeare.txt")
+    text = open(data_filename, 'r').read()
+    # let's treat every person's statement in the dialog as a separate document
+    text = "<|endoftext|>" + text
+    text = text.replace('\n\n', '\n\n<|endoftext|>')
+    # encode the text
+    tokens = encode(text)
+    tokens_np = np.array(tokens, dtype=np.int32)
+    # let's take the first 32,768 tokens as the validation split (~10%)
+    val_tokens_np = tokens_np[:32768]
+    train_tokens_np = tokens_np[32768:]
+    # save to file
+    val_filename = os.path.join(DATA_CACHE_DIR, "tiny_shakespeare_val.bin")
+    train_filename = os.path.join(DATA_CACHE_DIR, "tiny_shakespeare_train.bin")
+    with open(val_filename, "wb") as f:
+        f.write(val_tokens_np.tobytes())
+    with open(train_filename, "wb") as f:
