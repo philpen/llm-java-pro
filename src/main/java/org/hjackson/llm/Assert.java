f5455df7c028869d4333e8d2e2c24983072aa857
philpen <159920091+philpen@users.noreply.github.com>
2023-05-07 11:30:36 +0000

    Files.

--- src/main/java/org/hjackson/llm/Assert.java
+++ src/main/java/org/hjackson/llm/Assert.java
@@ -2,4 +2,4 @@ package org.hjackson.llm;
 public final class Assert {
     public static final float EPSILON = 1e-6f;
     public static void floatEquals(float a, float b) {
- 
+        float abs = Math.abs(
