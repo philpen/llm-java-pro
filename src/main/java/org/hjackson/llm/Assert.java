457785f7f4fc9ea54a46d059abd49f17aea7dc68
philpen <159920091+philpen@users.noreply.github.com>
2023-05-14 22:49:47 +0000

    Minor code cleanups

--- src/main/java/org/hjackson/llm/Assert.java
+++ src/main/java/org/hjackson/llm/Assert.java
@@ -3,4 +3,4 @@ public final class Assert {
     public static final float EPSILON = 1e-6f;
     public static void floatEquals(float a, float b) {
         float abs = Math.abs(a - b);
-        if(!
+        if(!nearlyEqual(a, b, EPSILON)) {
