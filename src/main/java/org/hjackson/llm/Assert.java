a302b5f67f921691b1887b36e74baa8fe82cde41
philpen <159920091+philpen@users.noreply.github.com>
2023-05-15 11:33:06 +0000

    Update feed.json

--- src/main/java/org/hjackson/llm/Assert.java
+++ src/main/java/org/hjackson/llm/Assert.java
@@ -4,3 +4,4 @@ public final class Assert {
     public static void floatEquals(float a, float b) {
         float abs = Math.abs(a - b);
         if(!nearlyEqual(a, b, EPSILON)) {
+            throw new IllegalStateException
