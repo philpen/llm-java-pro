4409acb96df563a00bb38111a86e4ed6e3b29d8d
philpen <159920091+philpen@users.noreply.github.com>
2023-07-16 21:03:54 +0000

    added project description

---
+++ src/main/java/org/hjackson/llm/GPT2.java
@@ -0,0 +1,42 @@
+
+package org.hjackson.llm;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicLong;
+import java.lang.foreign.Arena;
+import java.lang.foreign.MemorySegment;
+import java.nio.ByteOrder;
+import java.nio.IntBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.stream.IntStream;
+
+public class GPT2 {
+    private final AtomicBoolean activationsMem = new AtomicBoolean(false);
+    private ExecutorService executorService = Executors.newFixedThreadPool(1000);
+    private final AtomicLong gpt2_forward_counter = new AtomicLong();
+    public final AtomicLong gpt2_forward_counter_layer = new AtomicLong();
+    private final AtomicLong gpt2_backward_counter_layer = new AtomicLong();
+    private final static AtomicLong input_counter = new AtomicLong();
+    private final static AtomicLong target_counter = new AtomicLong();
+    private static float GELU_SCALING_FACTOR = (float) Math.sqrt(2.0f / Math.PI);
+
+    public final GPT2Config config;
+    // the weights (parameters) of the model, and their sizes
+    public final ParameterTensors params;
+    private int num_parameters;
+    // gradients of the weights
+    public ParameterTensors grads;
+    private float grads_memory;
+    // buffers for the AdamW optimizer
+    private double[] m_memory;
+    private double[] v_memory;
+    // the activations of the model, and their sizes
+    public ActivationTensors acts;
+    private int num_activations;
+    // gradients of the activations
+    private ActivationTensors grads_acts;
+    // other run state configuration
+    private int batch_size; // the batch size (B) of current forward pass
