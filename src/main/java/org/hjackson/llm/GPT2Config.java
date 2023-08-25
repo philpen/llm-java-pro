810d83d7822ada2a581b122b996098b8c973f4ca
philpen <159920091+philpen@users.noreply.github.com>
2023-08-25 11:04:57 +0000

    Add test coverage

--- src/main/java/org/hjackson/llm/GPT2Config.java
+++ src/main/java/org/hjackson/llm/GPT2Config.java
@@ -2,4 +2,5 @@ package org.hjackson.llm;
 import java.nio.IntBuffer;
 public final class GPT2Config {
     public int max_seq_len; // max sequence length, e.g. 1024
-    public int vocab_size; // vo
+    public int vocab_size; // vocab size, e.g. 50257
+    public int p
