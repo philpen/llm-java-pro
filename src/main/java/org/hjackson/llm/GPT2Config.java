dd703a0e38fca4bb9c37a692b642e670969023fb
philpen <159920091+philpen@users.noreply.github.com>
2023-08-25 10:08:17 +0000

    Change date.

--- src/main/java/org/hjackson/llm/GPT2Config.java
+++ src/main/java/org/hjackson/llm/GPT2Config.java
@@ -2,4 +2,4 @@ package org.hjackson.llm;
 import java.nio.IntBuffer;
 public final class GPT2Config {
     public int max_seq_len; // max sequence length, e.g. 1024
- 
+    public int vocab_size; // vo
