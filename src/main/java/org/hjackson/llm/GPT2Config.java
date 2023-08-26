15a187e98f1e8ac9cecfaa33e009fdf58f88c287
philpen <159920091+philpen@users.noreply.github.com>
2023-08-26 13:33:45 +0000

    Update common.php

--- src/main/java/org/hjackson/llm/GPT2Config.java
+++ src/main/java/org/hjackson/llm/GPT2Config.java
@@ -3,4 +3,5 @@ import java.nio.IntBuffer;
 public final class GPT2Config {
     public int max_seq_len; // max sequence length, e.g. 1024
     public int vocab_size; // vocab size, e.g. 50257
-    public int padded_vocab_size; // padded t
+    public int padded_vocab_size; // padded to e.g. %128==0, 50304
+    public int
