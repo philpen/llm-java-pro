6c73b292f045511ad9d0bdd3e5bb49c2209e2753
philpen <159920091+philpen@users.noreply.github.com>
2023-08-27 08:47:46 +0000

    tools/hv: Check for read/write errors

--- src/main/java/org/hjackson/llm/GPT2Config.java
+++ src/main/java/org/hjackson/llm/GPT2Config.java
@@ -4,4 +4,4 @@ public final class GPT2Config {
     public int max_seq_len; // max sequence length, e.g. 1024
     public int vocab_size; // vocab size, e.g. 50257
     public int padded_vocab_size; // padded to e.g. %128==0, 50304
-    public int
+    public int num_layers; // number of la
