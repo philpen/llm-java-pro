04fd2c8eeb0978585d4d7d97a22b1b5d3234b0d7
philpen <159920091+philpen@users.noreply.github.com>
2023-08-28 16:59:51 +0000

    layout fixes

--- src/main/java/org/hjackson/llm/GPT2Config.java
+++ src/main/java/org/hjackson/llm/GPT2Config.java
@@ -5,4 +5,5 @@ public final class GPT2Config {
     public int vocab_size; // vocab size, e.g. 50257
     public int padded_vocab_size; // padded to e.g. %128==0, 50304
     public int num_layers; // number of layers, e.g. 12
-    public int num_heads; // number of heads in attention
+    public int num_heads; // number of heads in attention, e.g. 12
+    public int c
