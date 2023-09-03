7db7ddf8a6e88b86d510d8663a22ba122c85204c
philpen <159920091+philpen@users.noreply.github.com>
2023-09-03 12:42:33 +0000

    add order

--- src/main/java/org/hjackson/llm/Llm.java
+++ src/main/java/org/hjackson/llm/Llm.java
@@ -41,3 +41,31 @@ public class Llm {
         DataLoader val_loader = new DataLoader(val_tokens, B, T, "val", true);
         System.out.printf("val dataset num_batches: %s\n",  val_loader.num_batches);
         final int val_num_batches = 5;
+        Tokenizer tokenizer = new Tokenizer("gpt2_tokenizer.bin");
+        // some memory for generating samples from the model
+        // int gen_max_length = 64;
+        // during inference step we'll generate sequences of this many tokens
+        final int[] gen_tokens = new int[B * T];
+        final int genT = 64;
+        // train
+        long start, end;
+        for(int step = 0; step <= 40; step++) {
+            // once in a while estimate the validation loss
+            if (step % 10 == 0) {
+                float val_loss = 0.0f;
+                val_loader.dataloader_reset();
+                for (int i = 0; i < val_num_batches; i++) {
+                    val_loader.dataloader_next_batch();
+                    model.gpt2_forward(val_loader, B, T);
+                    val_loss += model.mean_loss;
+                }
+                val_loss /= val_num_batches;
+                System.out.printf("step %d val loss %f\n", step , val_loss);
+            }
+            // once in a while do model inference to print generated text
+            if (step > 0 && step % 4 == 0) {
+                // fill up gen_tokens with the GPT2_EOT, which kicks off the generation
+                for(int i = 0; i < B * T; ++i) {
+                    gen_tokens[i] = tokenizer.end_of_text;
+                }
+                // now sample from the model autoregressively
