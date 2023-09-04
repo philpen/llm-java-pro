dbed7f731d463b037c1a5749be93961114dbe928
philpen <159920091+philpen@users.noreply.github.com>
2023-09-04 13:26:08 +0000

    Regenerate gemspec for version 0.4.3

--- src/main/java/org/hjackson/llm/ParameterTensors.java
+++ src/main/java/org/hjackson/llm/ParameterTensors.java
@@ -39,3 +39,39 @@ public class ParameterTensors {
     private final int lnfb;
     private final int lnfb_size;
     private final int num_params;
+    public final float[] mem;
+    private boolean ok = false;
+    private final Map<Integer, Float> tracking = new HashMap<>();
+    public ParameterTensors(MemorySegment segment, GPT2Config config) {
+        this(config);
+        int pos = 1024;//header
+        for (int i = 0; i < num_params; i++, pos += 4) {
+            mem[i] = segment.get(ValueLayout.JAVA_FLOAT.withOrder(ByteOrder.nativeOrder()), pos);
+        }
+        Assert.floatEquals(this.mem[0], -0.11010301f);
+        runParamAssertions();
+        ok = true;
+    }
+    public ParameterTensors(GPT2Config config) {
+        int maxT = config.max_seq_len;
+        int C = config.channels;
+        int V = config.vocab_size;
+        int Vp = config.padded_vocab_size;
+        int L = config.num_layers;
+        // allocate space for all the parameters and read them in
+        wte_size = Vp * C;
+        wte = 0;
+        wpe_size = maxT * C;
+        wpe = wte + wte_size;
+        ln1w_size = L * C;
+        ln1w = wpe + wpe_size;
+        ln1b_size = L * C;
+        ln1b = ln1w + ln1w_size;
+        qkvw_size = L * (3 * C) * C;
+        qkvw = ln1b + ln1b_size;
+        qkvb_size = L * (3 * C);
+        qkvb = qkvw + qkvw_size;
+        attprojw_size = L * C * C;
+        attprojw = qkvb + qkvb_size;
+        attprojb_size = L * C;
+        attprojb = attprojw + attprojw_size;
