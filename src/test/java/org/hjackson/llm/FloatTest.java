69d934d7b74843be24204b451bba9119770dad8c
philpen <159920091+philpen@users.noreply.github.com>
2023-11-12 12:32:03 +0000

    updates to readme

--- src/test/java/org/hjackson/llm/FloatTest.java
+++ src/test/java/org/hjackson/llm/FloatTest.java
@@ -9,4 +9,7 @@ public class FloatTest {
     public void randomTest() {
         float test = Random32.random_f32(1337l);
         System.out.printf("%1.17f", test);
-        Assertions.assertEquals(0.23031723
+        Assertions.assertEquals(0.23031723499298096f, test);
+    }
+
+    @
