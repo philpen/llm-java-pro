11b241a474a20ea0b10fd0418051ccb28902336a
philpen <159920091+philpen@users.noreply.github.com>
2023-12-17 19:57:07 +0000

    Remove unneeded variables

--- src/test/java/org/hjackson/llm/GPT2Test.java
+++ src/test/java/org/hjackson/llm/GPT2Test.java
@@ -31,3 +31,48 @@ class GPT2Test {
     private int cpos;
     private int n;
     private int Vp;
+
+    @BeforeEach
+    void setUp() throws Exception {
+
+        String stateFile = "gpt2_124M_debug_state.bin";
+        String checkpoint_path = "gpt2_124M.bin";
+        model = new GPT2(checkpoint_path);
+
+        this.C = model.config.channels;
+        this.V = model.config.vocab_size;
+        this.Vp = model.config.padded_vocab_size;
+        this.maxT = model.config.max_seq_len;
+        this.L = model.config.num_layers;
+
+        state_header = new int[256];
+        RandomAccessFile state_file = new RandomAccessFile(new File(stateFile), "r");
+        /**
+         * Reading the entire file into mem is orders of magnitude faster than looping over the file and reading it in.
+         */
+        mem = new byte[(int) state_file.length()];
+        cpos = 0;
+        state_file.read(mem);
+        byteBuffer = ByteBuffer.wrap(mem);
+        state_file.close();
+        for (int i = 0; i < 256; i++) {
+            state_header[i] = byteBuffer.order(ByteOrder.nativeOrder()).getInt();
+            cpos += 4;
+            Assertions.assertEquals(cpos, byteBuffer.position());
+        }
+        Assertions.assertEquals(20240327, state_header[0], "Bad magic state file");
+        Assertions.assertEquals(2, state_header[1], "Bad version in state file");
+        B = this.state_header[2]; // batch size, e.g. 4
+        T = this.state_header[3];
+        System.out.printf("[State]\n");
+        System.out.printf("batch_size: %d\n", B);
+        System.out.printf("seq_len: %d\n", T);
+    }
+
+    //@Test
+    void gpt2_build_from_checkpoint() throws IOException {
+        Assertions.assertEquals(20240327, state_header[0], "Bad magic state file");
+        Assertions.assertEquals(2, state_header[1], "Bad version in state file");
+        int[] x = new int[B * T];
+        int[] y = new int[B * T];
+        for (int i = 0; i < x.length; i++) {
