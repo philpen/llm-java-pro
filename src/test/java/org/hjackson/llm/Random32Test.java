920ce7d7ea89ada72fdd72535d496d205fdd43e9
philpen <159920091+philpen@users.noreply.github.com>
2023-12-31 22:55:02 +0000

    Update members.yml

--- src/test/java/org/hjackson/llm/Random32Test.java
+++ src/test/java/org/hjackson/llm/Random32Test.java
@@ -3,4 +3,5 @@ package org.hjackson.llm;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.random.RandomGen
+import java.util.random.RandomGenerator;
+import java.util.random.RandomGenerato
