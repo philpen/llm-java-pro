446e3c790898d12811bf82dd2c6949fb457855af
philpen <159920091+philpen@users.noreply.github.com>
2024-03-10 10:51:26 +0000

    Revert "net: update consumers of MSG_MORE to recognize MSG_SENDPAGE_NOTLAST"

--- verify/cfg.py
+++ verify/cfg.py
@@ -6,3 +6,37 @@ import numpy as np
 
 log = logging.getLogger(__name__)
 
+
+class Config:
+    headers = 256
+    header_size_b = headers * 4
+    vocab_size = 0
+    num_layers = 0
+    num_heads = 0
+    channels = 0
+
+
+class ParameterTensors:
+    def __init__(self, file_name, config):
+
+        f = open(file_name, 'rb')
+        f.seek(1024,0)
+
+        self.file_size = f.tell()
+        assert((self.file_size - 1024) % 4 == 0)
+        self.num_params = int((self.file_size - 1024)/4)
+        self.mem = np.fromfile(f, np.float32)
+        struct.unpack('f' * self.num_params, f.read(4 * self.num_params))
+        self.maxT = config.max_seq_len
+        self.C = config.channels
+        self.V = config.vocab_size
+        self.L = config.num_layers
+
+        self.wte_size = self.V * self.C
+        self.wte = 0
+
+        self.wpe_size = self.maxT * self.C
+        self.wpe = self.wte + self.wte_size
+
+        self.ln1w_size = self.L * self.C
+        self.ln1w = self.wpe + self.wpe_size
