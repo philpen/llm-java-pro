17692745537d8cf622b4af5b6a603853f8de7a30
philpen <159920091+philpen@users.noreply.github.com>
2024-03-16 22:04:25 +0000

    MAGETWO-31654: Magento Ui module code base clean up

--- verify/cfg.py
+++ verify/cfg.py
@@ -94,3 +94,44 @@ class ParameterTensors:
         assert(self.mem[self.wte + 5] == -0.078917674720287323)
 
         assert (self.mem[self.wpe] == -0.018820719793438911)
+        assert (self.mem[self.wpe + 1] == -0.197418600320816040)
+        assert (self.getMem(self.wpe + 5) == -0.105013281106948853)
+
+        assert (self.getMem(self.ln1w + 1) == 0.181958660483360291)
+
+        assert (self.getMem(self.ln1w + 5) == 0.194811657071113586)
+
+        assert (self.getMem(self.ln1b) == -0.003677325090393424)
+        assert (self.getMem(self.ln1b + 5) == -0.011468173004686832)
+
+        assert (self.getMem(self.qkvw) == -0.473848402500152588)
+        assert (self.getMem(self.qkvw + 5) == 0.032973293215036392)
+
+        assert (self.getMem(self.qkvb) == 0.480339139699935913)
+        assert (self.getMem(self.qkvb + 5) == -0.095427356660366058)
+
+        assert (self.getMem(self.attprojw) == 0.312718182802200317)
+        assert (self.getMem(self.attprojw + 5) == -0.437642186880111694)
+
+        assert (self.getMem(self.attprojb) == 0.150291591882705688)
+        assert (self.getMem(self.attprojb + 5) == -0.034447547048330307)
+
+        assert (self.getMem(self.ln2w) == 0.130966052412986755)
+        assert (self.getMem(self.ln2w + 5) == 1.269531369209289551)
+
+        assert (self.getMem(self.ln2b) == 0.042478270828723907)
+        assert (self.getMem(self.ln2b + 5) == -0.026806578040122986)
+
+        assert (self.getMem(self.fcw) == 0.094201952219009399)
+        assert (self.getMem(self.fcw + 5) == 0.051278203725814819)
+
+        assert (self.getMem(self.fcb) == 0.039619479328393936)
+        assert (self.getMem(self.fcb + 5) == -0.014704782515764236)
+
+        assert (self.getMem(self.fcprojw) == -0.106606408953666687)
+        assert (self.getMem(self.fcprojw + 5) == -0.105633556842803955)
+
+        assert (self.getMem(self.fcprojb) == 0.045023269951343536)
+        assert (self.getMem(self.fcprojb + 5) == -0.238876312971115112)
+
+        assert (self.getMem(self.lnfw) == 1.397080421447753906)
